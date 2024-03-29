2848d6636eb28df7023ab1d1328ade4e185fde03
Qoziwe <leka.false@gmail.com>
2024-03-29 22:00:38 +0600

    fix bugs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let c1 = 0,
 document.getElementById("card-button1").onclick = function () {
   if (c1 == 0) {
     if (window.innerWidth <= 700) {
-      document.querySelector(".card1").style.height = "55vw";
+      document.querySelector(".card1").style.height = "60vw";
       document.getElementById("card1").innerHTML =
         "Используйте надежные пароли и не используйте их повторно. Создайте сложные пароли, состоящие из комбинации букв, цифр и символов. Не используйте один и тот же пароль для нескольких аккаунтов. Лучше использовать менеджер паролей для хранения и генерации надежных паролей.";
       c1++;
@@ -38,7 +38,7 @@ document.getElementById("card-button1").onclick = function () {
 document.getElementById("card-button2").onclick = function () {
   if (c2 == 0) {
     if (window.innerWidth <= 700) {
-      document.querySelector(".card2").style.height = "55vw";
+      document.querySelector(".card2").style.height = "60vw";
       document.getElementById("card2").innerHTML =
         "Обновляйте программное обеспечение и антивирусное ПО. Периодически проверяйте наличие обновлений для всех установленных программ и операционной системы. Установите антивирусное программное обеспечение и регулярно сканируйте компьютер.";
       document.getElementById("card-button2").innerHTML = "Свернуть ->";
@@ -69,7 +69,7 @@ document.getElementById("card-button2").onclick = function () {
 document.getElementById("card-button3").onclick = function () {
   if (c3 == 0) {
     if (window.innerWidth <= 700) {
-      document.querySelector(".card3").style.height = "55vw";
+      document.querySelector(".card3").style.height = "60vw";
       document.getElementById("card3").innerHTML =
         "Будьте осторожны при открытии вложений и ссылок. Не открывайте непонятные письма с вложениями или подозрительные ссылки. Особенно аккуратно относитесь к письмам от неизвестных отправителей, содержащих просьбы о предоставлении личной информации.";
       document.getElementById("card-button3").innerHTML = "Свернуть ->";
@@ -100,7 +100,7 @@ document.getElementById("card-button3").onclick = function () {
 document.getElementById("card-button4").onclick = function () {
   if (c4 == 0) {
     if (window.innerWidth <= 700) {
-      document.querySelector(".card4").style.height = "55vw";
+      document.querySelector(".card4").style.height = "60vw";
       document.getElementById("card4").innerHTML =
         "Не передавайте личные данные по незащищенным каналам связи. Избегайте передачи личной информации через незащищенные Wi-Fi сети или открытые публичные сети. Лучше использовать VPN (виртуальную частную сеть) для обеспечения безопасности во время отправки конфиденциальной информации.";
       document.getElementById("card-button4").innerHTML = "Свернуть ->";
@@ -131,7 +131,7 @@ document.getElementById("card-button4").onclick = function () {
 document.getElementById("card-button5").onclick = function () {
   if (c5 == 0) {
     if (window.innerWidth <= 700) {
-      document.querySelector(".card5").style.height = "55vw";
+      document.querySelector(".card5").style.height = "60vw";
       document.getElementById("card5").innerHTML =
         "Включите двухфакторную аутентификацию. Двухфакторная аутентификация добавляет дополнительный уровень безопасности к вашему аккаунту, требуя второй метод проверки вашей личности, помимо пароля. Это может быть код, отправленный на ваш мобильный телефон или электронную почту.";
       document.getElementById("card-button5").innerHTML = "Свернуть ->";
@@ -162,7 +162,7 @@ document.getElementById("card-button5").onclick = function () {
 document.getElementById("card-button6").onclick = function () {
   if (c6 == 0) {
     if (window.innerWidth <= 700) {
-      document.querySelector(".card6").style.height = "55vw";
+      document.querySelector(".card6").style.height = "60vw";
       document.getElementById("card6").innerHTML =
         "Постоянно следите за безопасностью своих аккаунтов. Проверяйте активность входа в свои аккаунты, отслеживайте подозрительную активность и периодически меняйте пароли для обеспечения максимальной безопасности ваших личных данных.";
       document.getElementById("card-button6").innerHTML = "Свернуть ->";
@@ -195,7 +195,7 @@ const advisestyle = document.getElementById("advise");
 advisestyle.addEventListener("click", function () {
   if (window.innerWidth <= 700) {
     if (c1 == 1 || c2 == 1 || c3 == 1 || c4 == 1 || c5 == 1 || c6 == 1) {
-      advisestyle.style.height = "133vh";
+      advisestyle.style.height = "98vh";
     } else {
       advisestyle.style.height = "160vw";
     }
